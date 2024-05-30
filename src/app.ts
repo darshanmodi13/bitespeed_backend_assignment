575455e5f6e620fec203199d512508aba1c4555c
darshanmodi <darshanmodi@solvative.com>
2024-05-30 16:23:06 +0530

    feat(swagger update): updated some swagger configs

--- src/app.ts
+++ src/app.ts
@@ -72,10 +72,10 @@ export class App {
 			info: { ...spec.info, title: 'Documentation' },
 			servers: [
 				{
-					url: `https://bitespeed-backend-assignment-kj3f.onrender.com/api`,
+					url: `https://bitespeed-backend-assignment-kj3f.onrender.com`,
 				},
 				{
-					url: `http://localhost:${this.port}/api`,
+					url: `http://localhost:${this.port}`,
 				},
 			],
 		};
